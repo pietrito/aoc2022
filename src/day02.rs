5f4fe65a540ab5146d8ac0b58d955d66051a1982
pietrokt <fanikulapo@proton.me>
2022-12-29 20:13:16 +0100

    End of Day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,11 +1,33 @@
+/// A = X = Rock
+/// B = Y = Paper
+/// C = Z = Scissors
+
+fn round_score(other: char, me: char) -> u32 {
+    match (other, me) {
+        ('A', 'X') => 3 + 1,
+        ('A', 'Y') => 6 + 2,
+        ('A', 'Z') => 0 + 3,
+        ('B', 'X') => 0 + 1,
+        ('B', 'Y') => 3 + 2,
+        ('B', 'Z') => 6 + 3,
+        ('C', 'X') => 6 + 1,
+        ('C', 'Y') => 0 + 2,
+        ('C', 'Z') => 3 + 3,
+        _ => unreachable!(),
+    }
+}
+
 /// What would your total score be if everything goes exactly according to your strategy guide?
 pub fn part_1(input: &str) -> String {
     let mut score = 0;
     for line in input.trim().lines() {
-        let opponent = line.chars()[0];
-        let me = line.chars()[2];
+        let opponent = line.chars().nth(0).unwrap();
+        let me = line.chars().nth(2).unwrap();
+
+        score += round_score(opponent, me);
     }
-    todo!()
+
+    score.to_string()
 }
 
 pub fn part_2(input: &str) -> String {
@@ -16,19 +38,19 @@ pub fn part_2(input: &str) -> String {
 mod tests {
     use super::*;
 
-    const INPUT: &str = r#""#;
+    const INPUT: &str = r#"A Y
+B X
+C Z"#;
 
     #[test]
     fn test_example_1() {
-        assert_eq!(part_1(INPUT), "".to_string());
+        assert_eq!(part_1(INPUT), "15");
     }
 
-    /*
     #[test]
     fn test_part_1() {
-        assert_eq!(part_1(include_str!("../inputs/")), "".to_string());
+        assert_eq!(part_1(include_str!("../inputs/2")), "10994");
     }
-    */
 
     /*
     #[test]
